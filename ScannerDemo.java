caabfea69b1bb4c6a453c41bd08174945a710d06
WangswClover <wangsw.clover@gmail.com>
2016-05-03 21:14:17 +0800

    wrote a ScannerDemo.java file

---
+++ ScannerDemo.java
@@ -0,0 +1,18 @@
+package com.sevensec_01;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author sevensec
+ * @version 1.0
+ */
+
+public class ScannerDemo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		System.out.println("x="+x);
+	}
+}
